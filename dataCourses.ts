9c0c777800a5d26a6254f93f73a51e1ae03c56ae
Alejandro Mora Pinto <na.mora@uniandes.edu.co>
2020-10-21 20:34:08 -0500

    Configuracion inicial

---
+++ dataCourses.ts
@@ -0,0 +1,9 @@
+import { Course } from './course.js';
+
+export const dataCourses = [
+    new Course("Desarrollo en equipo SW ", "Rubby Casallas", 3),
+    new Course("Sistemas transaccionales", "German Bravo", 3),
+    new Course("Calculo vectorial", "Alexander Berenstein", 3),
+    new Course("Retos de la sustentabilidad", "Nathalia Franco", 3)
+
+  ]
---
+++ scripts/dataCourses.js
@@ -0,0 +1,7 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Desarrollo en equipo SW ", "Rubby Casallas", 3),
+    new Course("Sistemas transaccionales", "German Bravo", 3),
+    new Course("Calculo vectorial", "Alexander Berenstein", 3),
+    new Course("Retos de la sustentabilidad", "Nathalia Franco", 3)
+];
